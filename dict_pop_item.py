9138cfd4df45197f2f069fad9983a83b285cbf2c
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-03-04 23:45:55 +0530

    Add files via upload

---
+++ dict_pop_item.py
@@ -0,0 +1,4 @@
+animaldict={'name':'Dog','age':45,'weight':45,'country':'US','city':'California'}
+animaldicts=animaldict.pop('name')
+print(animaldicts)
+animaldict
---
+++ id_of_dict.py
@@ -0,0 +1,3 @@
+d={2:"red",4:'red',6:'jkl'}
+d.keys()
+print(id(d))

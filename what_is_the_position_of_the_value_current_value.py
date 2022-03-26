703ba44d064167af598a73776d43f20d2a8ac882
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-03-27 00:04:19 +0530

    Add files via upload

---
+++ what_is_the_position_of_the_value_current_value.py
@@ -0,0 +1,5 @@
+# The index() method only returns the first occurrence of the value.
+def index_of_ele(fruits):
+	x = fruits.index(32)
+	return x
+print(index_of_ele([4,1,2,55, 64, 32, 16, 32,3456,12]))

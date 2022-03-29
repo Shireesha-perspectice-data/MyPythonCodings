585834c409635108036584ff235be0bfc1ce027e
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-03-29 23:43:20 +0530

    Update In_given_list_each word_chars_count.py

--- In_given_list_each word_chars_count.py
+++ In_given_list_each word_chars_count.py
@@ -2,7 +2,7 @@ this_lst=["watermelon","blaky","hel","joky","hokey"]
 for item in this_lst:  #range(len(this_lst)):
     for each in item:
         print(item.count(each),each,end=" ")
-    print("")
+    print(" ")
 print("items")
     
     

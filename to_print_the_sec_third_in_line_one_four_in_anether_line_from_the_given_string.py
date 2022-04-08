e7ea1659db40b16974e5ad62233e3821d564aa9d
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-04-08 07:52:26 +0530

    Add files via upload

---
+++ to_print_the_sec_third_in_line_one_four_in_anether_line_from_the_given_string.py
@@ -0,0 +1,14 @@
+def stringTogether(s1,s2,s3,s4):
+	for each in s2:
+		print(each,end=" ")
+	print(end=' ')
+	for each in s3:
+		print(each,end=" ")
+	print()
+	for each in s1:
+		print(each,end=" ")
+	print(end=' ')
+	for each in s4:
+		print(each,end=' ')
+	print()
+print(stringTogether("DataScience","Python","JAva","c+"))

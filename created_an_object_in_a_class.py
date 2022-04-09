25b7a65cfb1753717e2ec0371869943cdd7cb13b
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-04-09 22:56:48 +0530

    Add files via upload

---
+++ created_an_object_in_a_class.py
@@ -0,0 +1,11 @@
+class created_obj:
+	def fun(self):
+		self.x1=23;
+class call_created(created_obj):
+	def fun_2(fun):
+		print("hey..")
+r=created_obj()
+# print(r.fun())
+# print(r.x1)  # to acces the current object by using calling the function.
+
+

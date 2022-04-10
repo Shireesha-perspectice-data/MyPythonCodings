f7e35fa0dd58ad634b02ecee8ffdf9ca53668316
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-04-10 23:40:50 +0530

    Add files via upload

--- dic_key_value_pair.py
+++ dic_key_value_pair.py
@@ -1,4 +1,4 @@
 def fun(words,values):
-	dic_a={i:j for i,j in zip(words,values)}
+	dic_a={i:j for i,j in zip(words,values) }
 	return dic_a
-print(fun(words=["Name","Age","month","Salary"],values=["Ram",22,8,650000]))
+print(fun(words=["Name","Age","Gender","month","Salary"],values=["Ram",22,"Male",8,650000]))

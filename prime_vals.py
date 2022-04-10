c37e744f16b1b538aaa4401a07c8e1dab9b81818
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-04-10 23:15:32 +0530

    Update prime_vals.py
    prime numbers

--- prime_vals.py
+++ prime_vals.py
@@ -2,10 +2,10 @@ def fun(my_list):
 	prime=[ ]
 	for i in range(my_list):
 		c=0
-		print(i,(c,"c val"),"i 1st")
+		print(i,(c,"c val"),"i value ")
 		print("---------")
 		for j in range(1,i):
-			print(j,"j 1st")
+			print(j,"j value ")
 			if i%j==0: # 1)2(2 here i=2,j=1
 				c+=1
 				print(j,"val",i)

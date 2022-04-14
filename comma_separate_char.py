2ccf67348531da814cc6ad7ea5ee4fd3bd9f75cf
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-04-14 23:55:59 +0530

    Update comma_separate_char.py

--- comma_separate_char.py
+++ comma_separate_char.py
@@ -1,6 +1,5 @@
-string="The man who is pure, and who dares, does great things."
-
-def fun():
+# string="The man who is pure, and who dares, does great things."
+def fun(string):
 	str_d=''
 	for i in string:
 		if i==' ':
@@ -9,4 +8,4 @@ def fun():
 			str_d=str_d+i;
 			#str_d+=str_d+i;
 	return str_d;
-print(fun())
+print(fun(string="The man who is pure, and who dares, does great things."))

68a74938eb496a356047d09617de7a6d5f9f7a82
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-04-20 23:00:44 +0530

    Update Add_an_ele_in_list.py

--- Add_an_ele_in_list.py
+++ Add_an_ele_in_list.py
@@ -1,5 +1,6 @@
 # create a list of elements
-#to add a element in a list
+#to add a element in a 
+
 list_e=[2,3,4,5,6,7,8,9]
 list_e.append(10) # to add the element to end of the list 
 print(list_e)

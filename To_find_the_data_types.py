636d81321850451867b4f1727e08fd2f91bc8b9e
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-04-22 22:41:48 +0530

    Update To_find_the_data_types.py

--- To_find_the_data_types.py
+++ To_find_the_data_types.py
@@ -1,8 +1,8 @@
 # To find the data types
-lq=["apple","mango","banana",9,2.4,76,"adb"]
+lq=["Apple","Mango","Banana",9,2.4,76,"adb","Ball"]
 ls=("abc","xys",2,2.6,5)
 ld={"age":24,"name":"Rakesh","salary":65000}
-lset={1,2,3,4,5,3,3,"abs","jkl"}
+lset={1,2,3,4,5,3,3,"abs","jan"}
 print(type(lq))
 print(type(ls))
 print(type(ld))

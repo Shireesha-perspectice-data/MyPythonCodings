28299d06536b08643103a00af985b2d071c1d5b0
Shireesha <99179617+Shireesha-perspective-data@users.noreply.github.com>
2022-04-23 00:07:25 +0530

    Update class_obj.py

--- class_obj.py
+++ class_obj.py
@@ -1,10 +1,10 @@
 class class_name:  # to create a class name is class_name
 	def class1(self):  # inside the class we are create a method, to access through the current object
-		self.x1=23;
+		self.x1=23;  #instance variable
 class class_roll_no(class_name):
 	def class2(self):
 		self.x2=34;
-ref_obj=class_name()
+ref_obj=class_name()   #created the reference variable
 print(ref_obj.class1())
 print(ref_obj.x1)
 
